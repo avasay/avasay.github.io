8977e8ebc5fbe02183c671e65b3965f47a3cabb4
Alejandrio Vasay <alejandrio.vasay@gmail.com>
2026-05-11 11:43:58 -0500

    Changed font style to make blog more  readable

--- js/anchorUtils.js
+++
@@ -1,9 +0,0 @@
-// This function scrolls to an element with the given ID
-function scrollToElement(elementId) {
-    const element = document.getElementById(elementId);
-    if (element) {
-        element.scrollIntoView({
-            behavior: 'smooth'
-        });
-    }
-}
